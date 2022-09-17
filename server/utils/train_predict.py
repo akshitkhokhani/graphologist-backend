7efaa4c2b83918a2ffea872c98bcffde9f7c9315
akshitkhokhani <akshitkhokhani20@gmail.com>
2022-09-17 14:47:01 +0530

    tkinter import remove

--- server/utils/train_predict.py
+++ server/utils/train_predict.py
@@ -1,7 +1,4 @@
 import os
-import itertools
-from tkinter.messagebox import RETRY
-import sklearn
 from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
